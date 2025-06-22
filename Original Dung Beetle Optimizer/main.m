171c1a9ba038ce0f3e9b24f621fc7569aa0a0472
changzhanyou0215 <112379739+changzhanyou0215@users.noreply.github.com>
2025-06-22 19:16:36 +0800

    Update main.m

--- Original Dung Beetle Optimizer/main.m
+++ Original Dung Beetle Optimizer/main.m
@@ -11,36 +11,26 @@
 % algorithm for global optimization. The Journal of Supercomputing, DOI:
 % 10.1007/s11227-022-04959-6
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-clear all
+clear all 
 close all
 clc
 
 SearchAgents_no=30; % Number of search agents
 
-Function_name='F8'; % Name of the test function that can be from F1 to F23 (Table 1,2,3 in the paper)
+Function_name='F1'; % Name of the test function that can be from F1 to F23 (Table 1,2,3 in the paper)
 
 Max_iteration=500; % Maximum numbef of iterations
 
 % Load details of the selected benchmark function
 [lb,ub,dim,fobj]=Get_Functions_details(Function_name);
-[DBO_fMin,DBO_bestX,DBO_curve]=DBO(SearchAgents_no,Max_iteration,lb,ub,dim,fobj);
-[Improved_DBO_fMin,Improved_DBO_bestX,Improved_DBO_curve]=Improved_DBO(SearchAgents_no,Max_iteration,lb,ub,dim,fobj);
-semilogy(DBO_curve,'Color','r')
-hold on
-semilogy(Improved_DBO_curve,'Color','g')
+[fMin,bestX,DBO_curve]=DBO(SearchAgents_no,Max_iteration,lb,ub,dim,fobj);
+semilogy(DBO_curve,'Color','g')
 title('Objective space')
 xlabel('Iteration');
 ylabel('Best score obtained so far');
 %axis tight
 grid on
 box on
-legend('DBO','Improved_DBO')
-display(['The best solution obtained by DBO is : ', num2str(DBO_bestX)]);
-display(['The best optimal value of the objective funciton found by DBO is : ', num2str(DBO_fMin)]);
-
-display(['The best solution obtained by Improved_DBO is : ', num2str(Improved_DBO_bestX)]);
-display(['The best optimal value of the objective funciton found by Improved_DBO is : ', num2str(Improved_DBO_fMin)]);
-
-
-
-
+legend('DBO')
+display(['The best solution obtained by DBO is : ', num2str(bestX)]);
+display(['The best optimal value of the objective funciton found by DBO is : ', num2str(fMin)]);
